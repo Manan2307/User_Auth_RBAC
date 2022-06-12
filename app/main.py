f2081986ee2de96a78e39d35f18fa9f559e43b75
Manan2307 <mananshah2307@gmail.com>
2022-06-12 22:30:16 +0530

    Created user

--- app/main.py
+++ app/main.py
@@ -22,3 +22,37 @@ class UserOut(BaseModel):
 @app.post("/user/", response_model=UserOut)
 async def create_user(user: UserIn):
     return user
+
+class AdminIn(BaseModel):
+    Adminname: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class AdminOut(BaseModel):
+    Adminname: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+@app.post("/Admin/", response_model=AdminOut)
+async def create_Admin(Admin: AdminIn):
+    return Admin
+
+class SuperAdminIn(BaseModel):
+    SuperAdminname: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class SuperAdminOut(BaseModel):
+    SuperAdminname: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+@app.post("/SuperAdmin/", response_model=SuperAdminOut)
+async def create_SuperAdmin(SuperAdmin: SuperAdminIn):
+    return SuperAdmin
